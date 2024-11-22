833920403bf5855addc63c493460c1c65bd44abd
dllll2 <wlsdud8658@gmail.com>
2024-11-22 15:21:16 +0900

    feat: 필수기능LV2추가

--- src/com/example/kiosk/LV1/Main.java
+++ src/com/example/kiosk/LV1/Main.java
@@ -1,28 +1,21 @@
-package com.example.kiosk.LV2;
+package com.example.kiosk.LV1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List <MenuItem> menuItem = new ArrayList<>();
-        menuItem.add(new MenuItem("ShackBurger",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem("SmokeShack",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem("Cheeseburger",6.9,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItem.add(new MenuItem("Hamburger",5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
-
         Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("[ SHAKESHACK MENU]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료      | 종료");
 
-        do{
-//            System.out.println("[ SHAKESHACK MENU]");
-//
-//            for(int i = 0; i < menuItem.size(); i++){
-//                System.out.printf("%d. %-15s W | %-2.2f | %s%n", i + 1 , menuItem.get(i).name, menuItem.get(i).price, menuItem.get(i).itemDetails);
-//            }
             char num = sc.next().charAt(0);
 
-            switch(num){
+            switch (num) {
                 case '1':
                     System.out.println("ShackBurger 를 선택하셨습니다.");
                     break;
@@ -43,7 +36,7 @@ public class Main {
                     break;
             }
 
-        }while(!sc.next().equals('0'));
+        } while (!sc.next().equals('0'));
 
         sc.close();
     }
