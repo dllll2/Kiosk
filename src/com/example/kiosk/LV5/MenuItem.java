725cdca402e3c3caf8a9ceb01fdfe11df631ac61
dllll2 <wlsdud8658@gmail.com>
2024-11-22 20:20:53 +0900

    feat: 새로운 패키지 생성

---
+++ src/com/example/kiosk/LV5/MenuItem.java
@@ -0,0 +1,27 @@
+package com.example.kiosk.LV5;
+
+public class MenuItem {
+    // 1. 속성(필드)
+    String name;
+    double price;
+    String itemDetails;
+
+    // 2. 생성자
+
+    /**
+     * MenuItem 생성자 선언
+     *
+     * @param name 음식 이름
+     * @param price 음식 가격
+     * @param itemDetails 음식 설명
+     */
+    public MenuItem(String name, double price, String itemDetails) {
+        this.name = name;
+        this.price = price;
+        this.itemDetails = itemDetails;
+    }
+
+
+    // 3. 기능 (메소드)
+
+}
